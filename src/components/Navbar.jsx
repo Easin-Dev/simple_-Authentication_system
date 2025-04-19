7656c43625eb6863d5bf820d46e06f842ab79938
Easin2023 <mdeasinarafat016456@gmail.com>
2025-04-19 16:02:13 +0600

    complete navbar and added git hub

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import { ShoppingCart } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  const navbarItems = [
+    { id: 1, name: "Shop", link: "/shop" },
+    { id: 2, name: "On Sale", link: "/one_sale" },
+    { id: 3, name: "New Arrivals", link: "/new_arrivals" },
+    { id: 4, name: "Brands  ", link: "/brands" },
+  ];
+
+  return (
+    <div className="flex justify-between items-center bg-white h-20 px-10 shadow-md">
+      <div>
+        <h1>SHOP.CO</h1>
+      </div>
+
+
+      <div className="flex items-center justify-between w-1/2">
+        <div>
+          {navbarItems.map((item) => (
+            <Link className="pr-4" key={item?.id} href={item?.link}>
+              {item?.name}
+            </Link>
+          ))}
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-[#F0F0F0] h-10 w-96 mt-3 rounded-r-full rounded-l-full p-4"
+          />
+        </div>
+      </div>
+      <div className="flex gap-4 items-center">
+        <button className="btn btn-circle">
+          <ShoppingCart />
+        </button>
+        <button className="btn btn-error text-white text-xl rounded-3xl">
+          Get in
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Navbar_offer_card.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar_offer_card = () => {
+    return (
+        <div>
+             <div className='bg-black text-white h-[38px] pt-2'>
+                <h1 className='text-sm text-center'>Sign up and get 20% off to your first order. <span className='underline cursor-pointer'>Sign Up Now</span></h1>
+             </div>
+        </div>
+    );
+};
+
+export default Navbar_offer_card;
